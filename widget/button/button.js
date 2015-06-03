f6014b643695d29c8d0b1303be5167f46ac872fc
Johnny Wu <KarasAya@gmail.com>
2015-06-03 09:53:20 +0800

    first commit

---
+++ widget/button/button.js
@@ -0,0 +1,9 @@
+Editor.registerWidget( 'editor-button', {
+    is: 'editor-button',
+
+    behaviors: [EditorUI.focusable],
+
+    ready: function () {
+        this._initFocusable( this.$.focus );
+    },
+});
