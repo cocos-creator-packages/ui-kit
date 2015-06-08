62424e01a3294ceb10a1a7bcc6f1896ca1fc4ac2
kamisama <1457434343@qq.com>
2015-06-08 12:21:38 +0800

    add editor-input

--- widget/input/input.js
+++ widget/input/input.js
@@ -1,17 +1,27 @@
 Editor.registerWidget( 'editor-input', {
     is: 'editor-input',
 
-    behaviors: [EditorUI.focusable],
+    behaviors: [EditorUI.focusable,Polymer.IronValidatableBehavior],
 
     properties: {
         placeholder: {
             type: String,
             notify: true,
             value: ''
+        },
+        value: {
+            type: String,
+            notify: true,
+            value: '',
+            observer: 'valueChanged'
         }
     },
 
     ready: function () {
         this._initFocusable(this.$.inputArea);
+    },
+
+    valueChanged: function () {
+        // TODO
     },
 });
