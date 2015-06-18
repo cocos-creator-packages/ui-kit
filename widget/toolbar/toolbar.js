2f2da40fc8af25c5b76a60c295c67c6b6cdcd424
kamisama <1457434343@qq.com>
2015-06-18 12:01:53 +0800

    fix https://github.com/fireball-packages/ui-kit/pull/50

--- widget/toolbar/toolbar.js
+++ widget/toolbar/toolbar.js
@@ -1,10 +1,3 @@
 Editor.registerWidget( 'editor-toolbar', {
     is: 'editor-toolbar',
-
-    behaviors: [EditorUI.focusable, Polymer.IronButtonState],
-    
-    ready: function () {
-        this.noNavigate = this.nofocus;
-        this._initFocusable(this);
-    },
 });
